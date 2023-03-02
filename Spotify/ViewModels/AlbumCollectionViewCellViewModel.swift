39fda36ea12ed4ed6c1c5b5a0584a13e6896898d
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2023-03-02 22:40:45 +0000

    - Build album view - Add date formatter to extensions

---
+++ Spotify/ViewModels/AlbumCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Aaron Johncock on 02/03/2023.
+//
+
+import Foundation
+
+struct AlbumCollectionViewCellViewModel {
+    let name: String
+    let artistName: String
+}
