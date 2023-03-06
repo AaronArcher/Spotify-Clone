d30cf574e9c7cf30f3709e271a1adc2ff78e2602
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2023-03-06 22:00:22 +0000

    - Build API Call to pull Categories - Build API Call to pull playlists from specific categories - Build UI for the categories view

---
+++ Spotify/ViewModels/CategoryCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Aaron Johncock on 06/03/2023.
+//
+
+import Foundation
+
+struct CategoryCollectionViewCellViewModel {
+    let title: String
+    let artworkURL: URL?
+}
